0ae46edb93113e41b8dd911d350333abeea3c368
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-06-14 12:42:08 -0300

    passwordReset

--- metricsEvent/handlers/passwordReset.js
+++ metricsEvent/handlers/passwordReset.js
@@ -1,11 +1,27 @@
-exports.handler = async (event) => {
-  const type = event?.type;
+const { Firestore, FieldValue } = require("@google-cloud/firestore");
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello!`,
-      type,
-    }),
-  };
+exports.handler = async (event) => {
+  try {
+    const type = event?.type;
+
+    const firestore = new Firestore();
+
+    firestore.doc("metrics/users").update({
+      passwordReset: FieldValue.increment(1),
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Successfully incremented ${type}`,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
 };
